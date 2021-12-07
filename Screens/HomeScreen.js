0d82a3cb057fa2ef23b4b350f6c0de92ad3d2247
nkangi jafari <nkangijafari@gmail.com>
2021-12-07 17:49:58 +0400

    Added a chat screen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { View, Text, Button } from 'react-native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
-      <Text>i AM THE HOME SCREEN</Text>
+      <Text>i AM THE HOME SCREEN at the moment</Text>
+      <Button
+        title='Go to Details'
+        onPress={() => {
+          navigation.navigate('Chat');
+        }}
+      />
     </View>
   );
 };
--- StackNavigator.js
+++ StackNavigator.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Screens/HomeScreen';
+import ChatScreen from './Screens/ChatScreen';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='Chat' component={ChatScreen} />
+    </Stack.Navigator>
   );
 };
 
