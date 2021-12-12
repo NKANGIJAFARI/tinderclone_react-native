b8a7a017a9b7bac1c694ba6d661973ca959a5eb4
nkangi jafari <nkangijafari@gmail.com>
2021-12-12 13:23:35 +0400

    Auth done

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import useAuth from '../Hooks/useAuth';
 
 const LoginScreen = () => {
+  const { signInWithGoogle } = useAuth();
+
   return (
     <View>
       <Text>This is the Login Screen</Text>
+      <Button title='Sign in with Google' onPress={signInWithGoogle} />
     </View>
   );
 };
---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestor } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBsWibs9rEyzgNaFgHAL-83bZP56Covb9g',
+  authDomain: 'tinder-clone-de76d.firebaseapp.com',
+  projectId: 'tinder-clone-de76d',
+  storageBucket: 'tinder-clone-de76d.appspot.com',
+  messagingSenderId: '794965708466',
+  appId: '1:794965708466:web:73951fe6bb20a9fb3503ab',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+export { auth, db };
