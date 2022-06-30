516cf70589fa38b2a040ed05f2f6bd66e26c6226
nkangi jafari <nkangijafari@gmail.com>
2022-06-30 09:00:43 +0400

    Shifting computers

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/core';
+import { View, Text, Button, ImageBackground } from 'react-native';
 import useAuth from '../Hooks/useAuth';
+import tw from 'tailwind-rn';
 
 const LoginScreen = () => {
   const { signInWithGoogle, loading } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
 
   return (
     <View>
-      <Text>{loading ? 'Loading....' : 'This is the Login Screen'}</Text>
-      <Button title='Sign in with Google' onPress={signInWithGoogle} />
+      <ImageBackground
+        resizeMode='cover'
+        style={tw('flex-1')}
+        source={{ url: 'https://tinder.com/static/tinder.png' }}>
+        <Text>{loading ? 'Loading....' : 'This is the Login Screen'}</Text>
+        <Button title='Sign in with Google' onPress={signInWithGoogle} />
+      </ImageBackground>
     </View>
   );
 };
